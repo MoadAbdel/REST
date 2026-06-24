446b66b419dae28b173428e42052be7d0276db45
moad abdel <waheb.moad@gmail.com>
2026-06-24 11:55:01 +0200

    activite 15

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,5 +1,16 @@
 import { prisma } from '../../app.js';
 
+export const readByHardness = async (req, res) => {
+  try {
+    const woods = await prisma.wood.findMany({
+      where: { hardness: req.params.hardness },
+    });
+    res.json(woods);
+  } catch (error) {
+    res.status(500).json({ error: error.message ?? 'An error occurred' });
+  }
+};
+
 export const readAll = async (req, res) => {
   try {
     const woods = await prisma.wood.findMany();
